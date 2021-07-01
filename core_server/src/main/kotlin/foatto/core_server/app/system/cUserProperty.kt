e9e766b15c9ab269fc4859805a3c6a26dc18a118
foatto <foatto@mail.ru>
2021-07-01 18:51:26 +0300

    - add user property editor module

---
+++ core_server/src/main/kotlin/foatto/core_server/app/system/cUserProperty.kt
@@ -0,0 +1,66 @@
+package foatto.core_server.app.system
+
+import foatto.core.app.UP_DECIMAL_DIVIDER
+import foatto.core.app.UP_IS_USE_THOUSANDS_DIVIDER
+import foatto.core.app.UP_TIME_OFFSET
+import foatto.core.link.FormData
+import foatto.core_server.app.server.cStandart
+import foatto.core_server.app.server.column.iColumn
+import foatto.core_server.app.server.data.DataBoolean
+import foatto.core_server.app.server.data.DataComboBox
+import foatto.core_server.app.server.data.DataRadioButton
+import foatto.core_server.app.server.data.iData
+
+class cUserProperty : cStandart() {
+
+    override fun definePermission() {
+        alPermission.add(Pair(PERM_ACCESS, "01 Access"))
+        alPermission.add(Pair(PERM_FORM, "02 Form"))
+        alPermission.add(Pair(PERM_ADD, "03 Add"))
+    }
+
+    override fun generateFormColumnData(id: Int, hmColumnData: MutableMap<iColumn, iData>) {
+        val mup = model as mUserProperty
+
+        (hmColumnData[mup.columnTimeShift] as DataComboBox).intValue = userConfig.upTimeOffset
+
+        (hmColumnData[mup.columnDivideThousands] as DataBoolean).value = userConfig.getUserProperty(UP_IS_USE_THOUSANDS_DIVIDER)?.toBoolean() ?: true
+
+        (hmColumnData[mup.columnDividerChar] as DataRadioButton).intValue = if (userConfig.getUserProperty(UP_DECIMAL_DIVIDER) == ",") {
+            1
+        } else {
+            0
+        }
+    }
+
+    override fun doSave(action: String, alFormData: List<FormData>, hmOut: MutableMap<String, Any>): String? {
+        val alColumnList = mutableListOf<iColumn>()
+        alColumnList.addAll(model.alFormHiddenColumn)
+        alColumnList.addAll(model.alFormColumn)
+
+        val hmColumnData = mutableMapOf<iColumn, iData>()
+
+        val id = getIDFromParam()!!
+
+        //--- ошибки ввода в форме
+        val returnURL = checkInput(id, alFormData, alColumnList, hmColumnData, hmOut)
+        //--- урло на возврат с ошибкой
+        if (returnURL != null) {
+            return returnURL
+        }
+
+        val mup = model as mUserProperty
+
+        userConfig.saveUserProperty(stm, UP_TIME_OFFSET, (hmColumnData[mup.columnTimeShift] as DataComboBox).intValue.toString())
+        userConfig.saveUserProperty(stm, UP_IS_USE_THOUSANDS_DIVIDER, (hmColumnData[mup.columnDivideThousands] as DataBoolean).value.toString())
+        userConfig.saveUserProperty(
+            stm, UP_DECIMAL_DIVIDER, if ((hmColumnData[mup.columnDividerChar] as DataRadioButton).intValue == 0) {
+                "."
+            } else {
+                ","
+            }
+        )
+
+        return "#"
+    }
+}
---
+++ core_server/src/main/kotlin/foatto/core_server/app/system/mUserProperty.kt
@@ -0,0 +1,87 @@
+package foatto.core_server.app.system
+
+import foatto.core.app.UP_DECIMAL_DIVIDER
+import foatto.core.app.UP_IS_USE_THOUSANDS_DIVIDER
+import foatto.core.app.UP_TIME_OFFSET
+import foatto.core_server.app.iApplication
+import foatto.core_server.app.server.AliasConfig
+import foatto.core_server.app.server.UserConfig
+import foatto.core_server.app.server.column.ColumnBoolean
+import foatto.core_server.app.server.column.ColumnComboBox
+import foatto.core_server.app.server.column.ColumnInt
+import foatto.core_server.app.server.column.ColumnRadioButton
+import foatto.core_server.app.server.mAbstract
+import foatto.sql.CoreAdvancedStatement
+
+class mUserProperty : mAbstract() {
+
+    private val arrTZOffset = arrayOf(
+        Pair(0, "UTC+00:00"),
+        Pair(1 * 60 * 60, "UTC+01:00"),
+        Pair(2 * 60 * 60, "UTC+02:00"),
+        Pair(3 * 60 * 60, "UTC+03:00"),
+        Pair(4 * 60 * 60, "UTC+04:00"),
+        Pair(5 * 60 * 60, "UTC+05:00"),
+        Pair(6 * 60 * 60, "UTC+06:00"),
+        Pair(7 * 60 * 60, "UTC+07:00"),
+        Pair(8 * 60 * 60, "UTC+08:00"),
+        Pair(9 * 60 * 60, "UTC+09:00"),
+        Pair(10 * 60 * 60, "UTC+10:00"),
+        Pair(11 * 60 * 60, "UTC+11:00"),
+        Pair(12 * 60 * 60, "UTC+12:00"),
+    )
+
+    lateinit var columnTimeShift: ColumnComboBox
+    lateinit var columnDivideThousands: ColumnBoolean
+    lateinit var columnDividerChar: ColumnRadioButton
+
+    override fun init(
+        application: iApplication,
+        aStm: CoreAdvancedStatement,
+        aliasConfig: AliasConfig,
+        userConfig: UserConfig,
+        aHmParam: Map<String, String>,
+        hmParentData: MutableMap<String, Int>,
+        id: Int?
+    ) {
+
+        super.init(application, aStm, aliasConfig, userConfig, aHmParam, hmParentData, id)
+
+        //----------------------------------------------------------------------------------------------------------------------
+
+        tableName = FAKE_TABLE_NAME
+
+        //----------------------------------------------------------------------------------------------------------------------
+
+        columnID = ColumnInt(tableName, "id").apply {
+            isVirtual = true
+        }
+
+        //----------------------------------------------------------------------------------------------------------------------
+
+        columnTimeShift = ColumnComboBox(tableName, UP_TIME_OFFSET, "Часовой пояс").apply {
+            isVirtual = true
+            arrTZOffset.forEach { (offset, descr) ->
+                addChoice(offset, descr)
+            }
+        }
+
+        columnDivideThousands = ColumnBoolean(tableName, UP_IS_USE_THOUSANDS_DIVIDER, "Разделять тысячи пробелами").apply {
+            isVirtual = true
+        }
+
+        columnDividerChar = ColumnRadioButton(tableName, UP_DECIMAL_DIVIDER, "Разделитель дробной части").apply {
+            isVirtual = true
+            addChoice(0, "Точка")
+            addChoice(1, "Запятая")
+        }
+
+        //----------------------------------------------------------------------------------------------------------------------
+
+        alFormHiddenColumn.add(columnID)
+
+        alFormColumn.add(columnTimeShift)
+        alFormColumn.add(columnDivideThousands)
+        alFormColumn.add(columnDividerChar)
+    }
+}
