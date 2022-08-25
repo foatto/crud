e92258ef75c0a41b19799ed3c95c2e504bb30706
foatto <foatto@mail.ru>
2022-08-25 20:52:38 +0300

    - core_server: comment unused UserDTO

--- core_server/src/main/kotlin/foatto/core_server/app/server/UserDTO.kt
+++ core_server/src/main/kotlin/foatto/core_server/app/server/UserDTO.kt
@@ -1,20 +1,20 @@
-package foatto.core_server.app.server
-
-class UserDTO(
-    val id: Int,
-    val parentId: Int,
-    val userId: Int,
-    val isDisabled: Boolean,
-    val orgType: Int,
-    val login: String,
-    val password: String,
-    val fullName: String,
-    val shortName: String,
-    val atCount: Int,
-    val lastLoginDateTime: Array<Int>,
-    val passwordLastChangeDate: Array<Int>,
-    val eMail: String,
-    val contactInfo: String,
-    val fileId: Int,
-    val lastIP: String,
-)
+//package foatto.core_server.app.server
+//
+//class UserDTO(
+//    val id: Int,
+//    val parentId: Int,
+//    val userId: Int,
+//    val isDisabled: Boolean,
+//    val orgType: Int,
+//    val login: String,
+//    val password: String,
+//    val fullName: String,
+//    val shortName: String,
+//    val atCount: Int,
+//    val lastLoginDateTime: Array<Int>,
+//    val passwordLastChangeDate: Array<Int>,
+//    val eMail: String,
+//    val contactInfo: String,
+//    val fileId: Int,
+//    val lastIP: String,
+//)
