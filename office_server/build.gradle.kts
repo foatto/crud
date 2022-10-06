9bd6b2c254793f4e905eabce1145f6cd2d0a3ab2
foatto <foatto@mail.ru>
2022-10-06 18:55:05 +0300

    - separate office_server module (with Sprint Boot) to 2 module: office_server & office_server_mvc (with Spring Boot MVC);

--- office_server/build.gradle.kts
+++ office_server/build.gradle.kts
@@ -4,25 +4,19 @@ val kotlinLanguageVersion: String by project
 val kotlinApiVersion: String by project
 val kotlinJvmTarget: String by project
 
+val officeServerVersion: String by project
+
 plugins {
     kotlin("jvm")
 
-    kotlin("plugin.spring")
-    kotlin("plugin.jpa")
-
-    id("io.spring.dependency-management")
-    id("org.springframework.boot")
+    `maven-publish`
 }
+
+version = officeServerVersion
 
 dependencies {
     api(project(":core_server"))
     api(project(":office_core"))
-}
-
-allOpen {
-    annotation("javax.persistence.Entity")
-    annotation("javax.persistence.MappedSuperclass")
-    annotation("javax.persistence.Embeddable")
 }
 
 tasks {
@@ -37,14 +31,5 @@ tasks {
     }
     jar {
         enabled = true
-    }
-    build {
-        doLast {
-            delete("/home/foatto/OfficeServerSpring/lib")
-            copy {
-                from("build/libs/office_server-plain.jar", configurations["runtimeClasspath"])
-                into("/home/foatto/OfficeServerSpring/lib")
-            }
-        }
     }
 }
---
+++ office_server_mvc/build.gradle.kts
@@ -0,0 +1,50 @@
+val isBuildSupressWarning: String by project
+
+val kotlinLanguageVersion: String by project
+val kotlinApiVersion: String by project
+val kotlinJvmTarget: String by project
+
+plugins {
+    kotlin("jvm")
+
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
+
+    id("io.spring.dependency-management")
+    id("org.springframework.boot")
+}
+
+dependencies {
+    api(project(":core_server_mvc"))
+    api(project(":office_server"))
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            languageVersion = kotlinLanguageVersion
+            apiVersion = kotlinApiVersion
+            jvmTarget = kotlinJvmTarget
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            suppressWarnings = isBuildSupressWarning.toBoolean()
+        }
+    }
+    jar {
+        enabled = true
+    }
+    build {
+        doLast {
+            delete("/home/foatto/OfficeServerSpring/lib")
+            copy {
+                from("build/libs/office_server_mvc-plain.jar", configurations["runtimeClasspath"])
+                into("/home/foatto/OfficeServerSpring/lib")
+            }
+        }
+    }
+}
