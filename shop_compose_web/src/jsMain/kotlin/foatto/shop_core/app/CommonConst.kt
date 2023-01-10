bcbbd591cbeee00408d7e7e158f90c421dc72bfd
foatto <foatto@mail.ru>
2023-01-10 17:12:48 +0300

    - core_compose_web: web application Shop Root class added;

---
+++ shop_compose_web/src/jsMain/kotlin/foatto/shop_core/app/CommonConst.kt
@@ -0,0 +1,19 @@
+package foatto.shop_core.app
+
+const val ICON_NAME_ADD_MARKED_ITEM = "shop_add_marked_item"
+const val ICON_NAME_FISCAL = "shop_fiscal_document"
+const val ICON_NAME_CALC = "shop_calc_document"
+
+const val ACTION_CASH_CALCULATOR = "cash_calculator"
+
+const val PARAM_DOC_ID = "doc_id"
+const val PARAM_DOC_COST = "doc_cost"
+const val PARAM_FISCAL_URL = "fiscal_url"
+const val PARAM_PRINT_URL = "print_url"
+
+const val CUSTOM_COMMAND_SAVE_DOC_PAYMENT = "custom_command_save_doc_payment"
+
+const val PARAM_DOC_PAYMENT_ID = "param_doc_payment_id"
+const val PARAM_DOC_PAYMENT_TERMIMAL = "param_doc_payment_termimal"
+const val PARAM_DOC_PAYMENT_SBERBANK = "param_doc_payment_sberbank"
+const val PARAM_DOC_PAYMENT_SERTIFICATE = "param_doc_payment_sertificate"
