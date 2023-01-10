3e2217f95adf61288b2e24e72fbcdccf2d9f7696
foatto <foatto@mail.ru>
2023-01-10 17:10:38 +0300

    - core_compose_web: Int value to CSSColorValue function added;

--- core_compose_web/src/jsMain/kotlin/foatto/core_compose_web/util/JSUtil.kt
+++ core_compose_web/src/jsMain/kotlin/foatto/core_compose_web/util/JSUtil.kt
@@ -4,11 +4,17 @@ import foatto.core_compose_web.external.SHA_1
 import foatto.core_compose_web.external.SHA_INPUT_TEXT
 import foatto.core_compose_web.external.SHA_OUTPUT_B64
 import foatto.core_compose_web.external.jsSHA
+import org.jetbrains.compose.web.css.CSSColorValue
+import org.jetbrains.compose.web.css.rgba
 
-fun getColorFromInt(argb: Int) =
 //--- старший байт прозрачности сначала двигаем, потом маскируем, т.к. он знаковый и может притащить за собой минус.
-    //--- заодно и другие байты сделаем так же
-    "rgba(${argb shr 16 and 0xFF},${argb shr 8 and 0xFF},${argb and 0xFF},${(argb shr 24 and 0xFF) / 255.0})"
+//--- заодно и другие байты сделаем так же
+fun getColorFromInt(argb: Int): CSSColorValue = rgba(
+    r = argb shr 16 and 0xFF,
+    g = argb shr 8 and 0xFF,
+    b = argb and 0xFF,
+    a = (argb shr 24 and 0xFF) / 255.0,
+)
 
 fun encodePassword(password: String): String {
     val sha = jsSHA(SHA_1, SHA_INPUT_TEXT)
