baa4362dd63138338b6ed3a967c3e8a15102f96c
foatto <foatto@mail.ru>
2023-01-11 22:09:52 +0300

    - core_compose: add common control interface;

---
+++ core_compose_web/src/jsMain/kotlin/foatto/core_compose_web/control/EmptyControl.kt
@@ -0,0 +1,11 @@
+package foatto.core_compose_web.control
+
+import androidx.compose.runtime.Composable
+
+class EmptyControl : iControl {
+
+    @Composable
+    override fun getBody() {
+
+    }
+}
---
+++ core_compose_web/src/jsMain/kotlin/foatto/core_compose_web/control/iControl.kt
@@ -0,0 +1,9 @@
+package foatto.core_compose_web.control
+
+import androidx.compose.runtime.Composable
+
+interface iControl {
+
+    @Composable
+    fun getBody()
+}
