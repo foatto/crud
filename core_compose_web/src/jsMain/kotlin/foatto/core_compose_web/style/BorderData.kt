d6935862f92e3131e7fafdda2aff8285ce982c97
foatto <foatto@mail.ru>
2023-01-12 22:55:48 +0300

    - core_compose_web: add no-border style definition;

--- core_compose_web/src/jsMain/kotlin/foatto/core_compose_web/style/BorderData.kt
+++ core_compose_web/src/jsMain/kotlin/foatto/core_compose_web/style/BorderData.kt
@@ -2,6 +2,7 @@ package foatto.core_compose_web.style
 
 import org.jetbrains.compose.web.css.CSSColorValue
 import org.jetbrains.compose.web.css.LineStyle
+import org.jetbrains.compose.web.css.hsl
 import org.jetbrains.compose.web.css.px
 
 class BorderData(
@@ -10,3 +11,11 @@ class BorderData(
     val width: CSSSize = 1.px,
     val radius: CSSSize,
 )
+
+val noBorder: BorderData = BorderData(
+    color = hsl(0, 0, 0),
+    style = LineStyle.None,
+    width = 0.px,
+    radius = 0.px
+)
+
