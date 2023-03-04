7d8baba0d486f246a44c0152e0cb182af6202211
foatto <foatto@mail.ru>
2023-03-04 16:59:09 +0300

    - core_compose_web: add new property for text align;

---
+++ core_compose_web/src/jsMain/kotlin/org/jetbrains/compose/web/css/properties/Text2.kt
@@ -0,0 +1,7 @@
+package org.jetbrains.compose.web.css.properties
+
+import org.jetbrains.compose.web.css.StyleScope
+
+fun StyleScope.verticalAlign(value: String) {
+    property("vertical-align", value)
+}
