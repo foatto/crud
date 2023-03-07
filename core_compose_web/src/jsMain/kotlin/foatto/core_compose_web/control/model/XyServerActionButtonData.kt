d8b07c6b77135c030d31f1191b985ca073e3a6fc
foatto <foatto@mail.ru>
2023-03-07 13:20:20 +0300

    - core_compose_web: add XyServerActionButtonData for server actions data class;

---
+++ core_compose_web/src/jsMain/kotlin/foatto/core_compose_web/control/model/XyServerActionButtonData.kt
@@ -0,0 +1,10 @@
+package foatto.core_compose_web.control.model
+
+class XyServerActionButtonData(
+    val id: Int,
+    val caption: String,
+    val tooltip: String,
+    val icon: String,
+    val url: String,
+    val isForWideScreenOnly: Boolean,
+)
