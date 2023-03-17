dfc3487a7db8afceab4a87cfb0987c03de8a6152
foatto <foatto@mail.ru>
2023-03-17 13:07:22 +0300

    - core: move adapted core classes from core-Compose_web to core;

--- ts_compose_web/src/jsMain/kotlin/foatto/ts_core/app/CommandStatusCode.kt
+++
@@ -1,7 +0,0 @@
-package foatto.ts_core.app
-
-object CommandStatusCode {
-    const val NOT_SENDED = 0
-    const val SENDED = 1
-    const val DELETED = 2
-}
--- ts_compose_web/src/jsMain/kotlin/foatto/ts_core/app/CommonConst.kt
+++
@@ -1,5 +0,0 @@
-package foatto.ts_core.app
-
-const val ICON_NAME_TROUBLE_TYPE_CONNECT = "ts_object_trouble_type_connect"
-const val ICON_NAME_TROUBLE_TYPE_WARNING = "ts_object_trouble_type_warning"
-const val ICON_NAME_TROUBLE_TYPE_ERROR = "ts_object_trouble_type_error"
