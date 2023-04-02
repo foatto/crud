f38ed22bb718de84bb1f6adf4c5089e410f00a19
foatto <foatto@mail.ru>
2023-04-02 17:10:01 +0300

    - core_compose_web: descrease refresh variants for mobile layout;

--- core_compose_web/src/jsMain/kotlin/foatto/core_compose_web/control/composable/getRefreshSubToolbar.kt
+++ core_compose_web/src/jsMain/kotlin/foatto/core_compose_web/control/composable/getRefreshSubToolbar.kt
@@ -5,10 +5,12 @@ import androidx.compose.runtime.MutableState
 import foatto.core_compose_web.control.getColorRefreshButtonBack
 import foatto.core_compose_web.control.getStyleToolbarButtonBorder
 import foatto.core_compose_web.style.arrStyleCommonMargin
+import foatto.core_compose_web.style.getStyleIconNameSuffix
 import foatto.core_compose_web.style.setBorder
 import foatto.core_compose_web.style.setMargins
 import foatto.core_compose_web.style.styleCommonButtonFontSize
 import foatto.core_compose_web.style.styleIconButtonPadding
+import foatto.core_compose_web.style.styleIsNarrowScreen
 import org.jetbrains.compose.web.attributes.disabled
 import org.jetbrains.compose.web.css.backgroundColor
 import org.jetbrains.compose.web.css.cursor
@@ -24,11 +26,16 @@ fun getRefreshSubToolbar(
     onButtonClick: (interval: Int) -> Unit
 ) {
     getToolBarSpan {
-        // 1s-interval shortly too
-        listOf(0, /*1,*/ 5, 10, 30).forEach { interval ->
+        // 1s-interval shortly too for all devices
+        // 5s-interval shortly too for mobile devices
+        if (styleIsNarrowScreen) {
+            listOf(0, /*1, 5,*/ 10, 30)
+        } else {
+            listOf(0, /*1,*/ 5, 10, 30)
+        }.forEach { interval ->
             val isEnabledButton = (interval == 0 || interval != refreshInterval.value)
             Img(
-                src = "/web/images/ic_replay_${if (interval == 0) "" else "${interval}_"}black_48dp.png",
+                src = "/web/images/ic_replay_${if (interval == 0) "" else "${interval}_"}${getStyleIconNameSuffix()}.png",
                 attrs = {
                     style {
                         fontSize(styleCommonButtonFontSize)
